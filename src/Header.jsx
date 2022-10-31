9d175a5aab497a289c47532c9c8df1a51c4456ee
AndersMeinicke <andersmeinicke@gmail.com>
2022-10-31 13:12:00 +0100

    yes

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {NavLink} from "react-router-dom"
+import "../src/styles/Header.css"
+function Header(props){
+    return(
+        <nav className="navbar">
+            <NavLink className="active" to="/"><i className="fa fa-fw fa-home"></i> Home</NavLink>
+            <NavLink to="/search"><i className="fa fa-fw fa-search"></i> Search</NavLink>
+            <NavLink to="/contact"><i className="fa fa-fw fa-envelope"></i> Contact</NavLink>
+            <NavLink to="/login"><i className="fa fa-fw fa-user"></i> Login</NavLink>
+        </nav>
+    )
+}
+export default Header;
